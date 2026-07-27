3a765ce244ae2aec92f855e07c31b71b7d1e8fdc
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-07-28 00:43:52 +0500

    Update story_generator.py

--- modules/story_generator.py
+++ modules/story_generator.py
@@ -1,1 +1,32 @@
+from groq import Groq
+import streamlit as st
 
+from config.prompts import SYSTEM_PROMPT
+
+
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
+)
+
+
+def generate_story(user_prompt):
+
+    response = client.chat.completions.create(
+
+        model="llama-3.3-70b-versatile",
+
+        temperature=0.8,
+
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
