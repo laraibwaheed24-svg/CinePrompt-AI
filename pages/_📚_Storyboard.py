54a0a2b5d41ff06c964db62eab2bee4c9b2678b0
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-07-28 20:21:05 +0500

    Update _📚_Storyboard.py

--- pages/_📚_Storyboard.py
+++ pages/_📚_Storyboard.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Storyboard Studio",
+    page_icon="📚",
+    layout="wide"
+)
+
+st.title("📚 Storyboard Studio")
+
+st.info(
+    "Your generated movie scenes will appear here."
+)
