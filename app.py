98c105e8189cee8e0e93a4b0c710717904f13409
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-07-28 15:53:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from components.movie_header import display_movie_header
 from components.sidebar import display_sidebar
 from modules.image_generator import generate_image
 
+if "movie" not in st.session_state:
+    st.session_state.movie = None
+
 # -----------------------------
 # Page Configuration
 # -----------------------------
