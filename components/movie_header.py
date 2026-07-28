7d401d31578a76857aed31054195e95d72511db1
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-07-28 17:39:47 +0500

    Update movie_header.py

--- components/movie_header.py
+++ components/movie_header.py
@@ -1,9 +1,31 @@
 import streamlit as st
+from modules.poster_generator import generate_movie_poster
 
 
 def display_movie_header(movie):
 
+    poster_key = "movie_poster"
+
+    if poster_key not in st.session_state:
+        st.session_state[poster_key] = None
+
+
     st.title(f"🎬 {movie['movie_title']}")
+
+    if st.button("🎨 Generate Movie Poster"):
+
+        with st.spinner("Creating movie poster..."):
+
+            st.session_state[poster_key] = generate_movie_poster(movie)
+
+    if st.session_state[poster_key]:
+
+        st.image(
+            st.session_state[poster_key],
+            caption="AI Movie Poster",
+            use_container_width=True,
+        )
+    
 
     col1, col2, col3 = st.columns(3)
 
